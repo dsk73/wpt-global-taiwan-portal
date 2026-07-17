058144b0689b0d75c5f2e89295a6823fd6af4d4e
dsk73 <dhananjayskokate@gmail.com>
2026-07-17 17:50:44 +0530

    hero test1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,4 @@
 import Hero from '@/features/hero/Hero';
-import Ambassadors from '@/features/ambassadors/Ambassadors';
-import Payment from '@/features/payment/Payment';
-import Activities from '@/features/activities/Activities';
-import FAQ from '@/features/faq/FAQ';
-import Footer from '@/features/footer/Footer';
 
 type Props = {
   params: Promise<{
@@ -14,19 +9,5 @@ type Props = {
 export default async function HomePage({ params }: Props) {
   const { locale } = await params;
 
-  return (
-    <>
-      <Hero locale={locale} />
-
-      <Ambassadors locale={locale} />
-
-      <Payment locale={locale} />
-
-      <Activities locale={locale} />
-
-      <FAQ locale={locale} />
-
-      <Footer locale={locale} />
-    </>
-  );
+  return <Hero locale={locale} />;
 }
